7f2dacf6b54b9eeee35bb1a06bd74f967650f9fa
Evan Wilde <theodore111@yahoo.com>
2014-04-04 16:05:05 -0700

    Initial commit

---
+++ license.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#######################################################################################
+# license
+#
+# license.py
+# Written by Evan Wilde
+# 
+# This program will place license agreement information at the top of source code files
+# It reads your information from a config file, or requests your information to generate
+# the config file
+#######################################################################################
+
+
+
+email_pattern = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+
+class Programmer:
+	name = ""
+	email = ""
+		
+
+
