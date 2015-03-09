fd31284f170151916988fbb45677c7de55eaf810
Evan Wilde <etcwilde@uvic.ca>
2015-03-08 22:07:05 -0700

    Adding Recursive configuration search

--- files.py
+++ files.py
@@ -1,4 +1,4 @@
-#######################################################################################
+##############################################################################
 # files
 #
 # files.py
@@ -6,130 +6,135 @@
 # April 8 2014
 #
 # Gives a simple interface for getting properties on a file
-#######################################################################################
+###############################################################################
 
 import re
 import os
 import time
+
 
 # File Property
 #
 # An object capable of returning contents of a file
 #
 class fileProperty:
-	"""docstring for fileProperty"""
+    """docstring for fileProperty"""
 
-	__file_name_pattern = re.compile("^.*/(.*)\.(.*)$")
+    __file_name_pattern = re.compile("^.*/(.*)\.(.*)$")
 
-	def __init__(self, filepath):
-		"""Creates a new file property"""
-		self.__filepath = ""
-		self.__filecreatetime = None
-		self.__filepath = os.path.abspath(filepath)
-		try:
-			self.__filecreatetime = time.strftime("%b %d %Y", time.localtime(os.path.getctime(filepath)))
-		except Exception:
-			pass
+    def __init__(self, filepath):
+        """Creates a new file property"""
+        self.__filepath = ""
+        self.__filecreatetime = None
+        self.__filepath = os.path.abspath(filepath)
+        try:
+            self.__filecreatetime = time.strftime("%b %d %Y",
+                                                  time.localtime(
+                                                      os.path.getctime(filepath)
+                                                  )
+                                                  )
+        except Exception:
+            pass
 
+    def __eq__(self, other):
+        """
+        Determines if two files are the same
+        Files are equal if the absolute filepath is the same.
+        """
+        return self.get_filepath() == other.get_filepath()
 
-	def __eq__(self, other):
-		"""
-		Determines if two files are the same
-		Files are equal if the absolute filepath is the same.
-		"""
-		return self.get_filepath() == other.get_filepath()
+    def __hash__(self):
+        """Hashing function for fileproperties
+        This is the result of the python string hash function on the filepath"""
+        return hash(self.__filepath)
 
-	def __hash__(self):
-		"""Hashing function for fileproperties
-		This is the result of the python string hash function on the filepath"""
-		return hash(self.__filepath)
+    def __iter__(self):
+        """Iterates through each line of the file"""
+        f = self.open()
+        if f:
+            for line in f:
+                yield line.strip('\n\r')
+            f.close()
+        else:
+            yield ""
 
-	def __iter__(self):
-		"""Iterates through each line of the file"""
-		f = self.open()
-		if f:
-			for line in f:
-				yield line.strip('\n\r')
-			f.close()
-		else:
-			return ""
+    def exists(self):
+        """Returns if the file exists and is accessible"""
+        try:
+            f = open(self.__filepath)
+            f.close()
+            return True
+        except IOError:
+            return False
 
-	def exists(self):
-		"""Returns if the file exists and is accessible"""
-		try:
-			f = open(self.__filepath)
-			f.close()
-			return True
-		except FileNotFoundError:
-			return False
+    def open(self, mode='r', buffering=-1, encoding=None, errors=None,
+             newline=None, closefd=True, opener=None):
+        """Returns an opened file"""
+        try:
+            return open(self.__filepath, mode, buffering, encoding, errors,
+                        newline, closefd, opener)
+        except IOError:
+            print ("Could not open file: %s" % self.__filepath)
+            return None
 
+    def get_lines(self):
+        """returns all the lines in the file"""
+        f = self.open()
+        if f:
+            for line in f:
 
-	def open(self, mode = 'r', buffering = -1, encoding = None, errors = None, newline = None, closefd = True, opener = None):
-		"""Returns an opened file"""
-		try:
-			return open(self.__filepath, mode, buffering, encoding, errors, newline, closefd, opener)
-		except FileNotFoundError:
-			print ("Could not open file: %s" % self.__filepath)
-			return None
+                yield str.strip(line)
+            f.close()
+        else:
+            yield ""
 
-	def get_lines(self):
-		"""returns all the lines in the file"""
-		f = self.open()
-		if f:
-			for line in f:
+    def get_filepath(self):
+        """Returns the absolute filepath"""
+        return self.__filepath
 
-				yield str.strip(line)
-			f.close()
-		else:
-			return ""
+    def get_filename(self):
+        """Returns the filename without extension or path"""
+        (filename, _) = self.__file_name_pattern.match(self.__filepath).groups()
+        return filename
 
+    def get_file(self):
+        """Returns the filename and extension"""
+        try:
+            (filename, file_extension) = self.__file_name_pattern.match(
+                self.__filepath).groups()
+        except AttributeError:
+            print ("No file extension!")
+        return filename + "." + file_extension
 
-	def get_filepath(self):
-		"""Returns the absolute filepath"""
-		return self.__filepath
+    def get_extension(self):
+        """Returns the extension of the file"""
+        try:
+            (_, file_extension) = self.__file_name_pattern.match(
+                self.__filepath).groups()
+        except AttributeError:
+            print ("No file extension")
+        return file_extension
 
-	def get_filename(self):
-		"""Returns the filename without extension or path"""
-		(filename, _) = self.__file_name_pattern.match(self.__filepath).groups()
-		return filename
+    def get_ctime(self):
+        """Returns last modified time of a file"""
+        return self.__filecreatetime
 
-	def get_file(self):
-		"""Returns the filename and extension"""
-		try:
-			(filename, file_extension) = self.__file_name_pattern.match(self.__filepath).groups()
-		except AttributeError:
-			print ("No file extension!")
-		return filename + "." + file_extension
+    def set_file_pattern(self, pattern):
+        """Sets the file search pattern for, allowing for different file patterns.
+        If the pattern is unable to compile, it will revert to the previous
+        pattern."""
+        old_pattern = self.__file_name_pattern
+        try:
+            new_pattern = re.compile(pattern)
+        except Exception:
+            print ("Error: Pattern Could Not Be Compiled")
+            new_pattern = old_pattern
+        self.__file_name_pattern = new_pattern
 
-	def get_extension(self):
-		"""Returns the extension of the file"""
-		try:
-			(_, file_extension) = self.__file_name_pattern.match(self.__filepath).groups()
-		except AttributeError:
-			print ("No file extension")
-		return file_extension
+    def __str__(self):
+        """String representation of the file data"""
+        return str(self.get_file())
 
-	def get_ctime(self):
-		"""Returns last modified time of a file"""
-		return self.__filecreatetime
-
-	def set_file_pattern(self, pattern):
-		"""Sets the file search pattern for, allowing for different file patterns.
-		If the pattern is unable to compile, it will revert to the previous pattern."""
-		old_pattern = self.__file_name_pattern
-		try:
-			new_pattern = re.compile(pattern)
-		except Exception:
-			print ("Error: Pattern Could Not Be Compiled")
-			new_pattern = old_pattern
-		self.__file_name_pattern = new_pattern
-
-	def __str__(self):
-		"""String representation of the file data"""
-		return str(self.get_file())
-
-	def __repr__(self):
-		"""A pretty representation of the file data"""
-		return str(self.get_file())
-
-
+    def __repr__(self):
+        """A pretty representation of the file data"""
+        return str(self.get_file())
--- header.py
+++ header.py
@@ -1,4 +1,4 @@
-#######################################################################################
+###############################################################################
 # header
 #
 # header.py
@@ -6,50 +6,53 @@
 # April 6 2014
 #
 # Contains the heading information class
-#######################################################################################
+###############################################################################
 import files
 
+
 class header:
-	"""Heading Data Container"""
-	__username = ""
-	__email = ""
-	__file_data = None
-	
-	def __init__(self, username, email, filepath):
-		"""Create a new header object"""
-		self.__username = username
-		self.__email = email
+    """Heading Data Container"""
+    __username = ""
+    __email = ""
+    __file_data = None
 
-		self.__file_data = files.fileProperty(filepath)
+    def __init__(self, username, email, filepath):
+        """Create a new header object"""
+        self.__username = username
+        self.__email = email
 
-	def get_username(self):
-		"""Returns the name that will be applied to the source headers"""
-		return self.__username
+        self.__file_data = files.fileProperty(filepath)
 
-	def get_email(self):
-		"""Returns the email of the current user"""
-		return self.__email
+    def get_username(self):
+        """Returns the name that will be applied to the source headers"""
+        return self.__username
 
-	def get_filepath(self):
-		"""Returns the absolute filepath"""
-		return self.__file_data.get_filepath()
+    def get_email(self):
+        """Returns the email of the current user"""
+        return self.__email
 
-	def get_filename(self):
-		"""Returns the filename without extension or path"""
-		return self.__file_data.get_filename()
+    def get_filepath(self):
+        """Returns the absolute filepath"""
+        return self.__file_data.get_filepath()
 
-	def get_file(self):
-		"""Returns the filename and extension"""
-		return self.__file_data.get_file()
+    def get_filename(self):
+        """Returns the filename without extension or path"""
+        return self.__file_data.get_filename()
 
-	def get_extension(self):
-		"""Returns the extension of the file"""
-		return self.__file_data.get_extension()
+    def get_file(self):
+        """Returns the filename and extension"""
+        return self.__file_data.get_file()
 
-	def get_create_time(self):
-		"""Returns the last modified time"""
-		return self.__file_data.get_ctime()
-	
-	def __repr__(self):
-		"""A pretty representation of the header data"""
-		return "{0} <{1}>\nFilename: {2} -- {4}\nExtension: {3}".format(self.__username, self.__email, self.get_filename(), self.get_extension(), self.get_create_time())
+    def get_extension(self):
+        """Returns the extension of the file"""
+        return self.__file_data.get_extension()
+
+    def get_create_time(self):
+        """Returns the last modified time"""
+        return self.__file_data.get_ctime()
+
+    def __repr__(self):
+        """A pretty representation of the header data"""
+        return "{0} <{1}>\nFilename: {2} -- {4}\nExtension: {3}".format(
+            self.__username, self.__email, self.get_filename(),
+            self.get_extension(), self.get_create_time())
